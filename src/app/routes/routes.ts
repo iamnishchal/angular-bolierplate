8dbc6b147e9d8905af379228f346ad694f0ee206
Nishchal <nishchal.p@hashedin.com>
2020-01-15 20:14:38 +0530

    Added a default route

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,6 +3,11 @@ import { Router } from '@angular/router';
 
 export const ROUTES = [
   {
+    path: '',
+    redirectTo: 'samplemodule',
+    pathMatch: 'full'
+  },
+  {
     path: 'samplemodule',
     loadChildren: './../modules/sample-module/sample-module.module#SampleModuleModule',
     canActivate: []
